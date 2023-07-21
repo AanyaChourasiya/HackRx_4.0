12e9168701dc1e4d5078790d3a2a180a44b571ba
Ujjawalsahu <ujjawalsahu121@gmail.com>
2023-07-21 16:31:26 +0530

    adding files in contain folder

--- Main/client/src/Contain/Third.js
+++ Main/client/src/Contain/Third.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {ColorType,createChart } from 'lightweight-charts'
+function Third(props) {
 
-function Third() {
+    const chartContainerRef = useRef();
+    const[products,setProducts] = useState();
+
+    console.log(props.name);
+
+    useEffect(()=>{
+        const getProducts = async
+    })
+
   return (
     <div>
       
