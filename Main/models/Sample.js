9670860e4dfd26601148c17c4dd996e9607c5806
Ujjawalsahu <ujjawalsahu121@gmail.com>
2023-07-21 13:04:20 +0530

    we created a mongodb model to connect with API

--- Main/models/Sample.js
+++ Main/models/Sample.js
@@ -3,4 +3,14 @@ const mongoose = require("mongoose")
 const SampleSchema = new mongoose.Schema({
     token: Number,
     exchid : Number,
+    open : Number,
+    high : Number,
+    low : Number,
+    close : Number,
+    volume : Number,
+    tradeDate : String,
+    symbol : String
 })
+
+const SampleModel = mongoose.model("Sample","SampleSchema")
+module.exports = SampleModel
