5982b811a7cd20e500f141d807a9af3bab235e62
Ujjawalsahu <ujjawalsahu121@gmail.com>
2023-07-21 16:45:36 +0530

    converting date

--- Main/client/src/Contain/Third.js
+++ Main/client/src/Contain/Third.js
@@ -8,7 +8,22 @@ function Third(props) {
     console.log(props.name);
 
     useEffect(()=>{
-        const getProducts = async
+        const getProducts = async ()=>{
+            const response = await fetch(`http://localhost:80/getSample/name${props.name}`);
+            const data = await response.json();
+            setProducts(data);
+        };
+        getProducts();
+    },[props.name]);
+
+    useEffect(()=>{
+        if(products && products.length > 0){
+            const validData = products.filter(item => item.tradeDate !== undefined && item.tradeDate !== null);
+
+            const initialData = validData.map(item => {
+                const parts = items.tradeDate.split
+            })
+        }
     })
 
   return (
