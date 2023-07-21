6eadc7a085d5368e05a8025be05e9b132e9f097c
Ujjawalsahu <ujjawalsahu121@gmail.com>
2023-07-21 15:49:17 +0530

    Creating a navigation bar

--- Main/client/src/Contain/Navigation.js
+++ Main/client/src/Contain/Navigation.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function Navigation() {
+
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(
+        'http://localhost:80/getSample/28-06-2023'
+      );
+      const data = await response.json();
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
+  console.log(products);
+
   return (
-    <div>
-      
+    <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    {/* <a className="navbar-brand" href="#">Navbar</a> */}
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="molo nav-link active" aria-current="page" target='blank' href="https://www.moneycontrol.com/india/stockpricequote/finance-nbfc/bajajfinance/BAF"><strong>NIFTY 50</strong></a>
+        </li>
+        <div className="Success">
+        <li className="nav-item">
+          <a className=" colo nav-link " href="#">19,711.45</a>
+        </li>
+        </div>
+        <li className="nav-item">
+          <a className="nav-link rolo  " href="#">+146.95(0.75%) </a>
+        </li>
+        
+        <li className="nav-item">
+          <a className="molo nav-link active" aria-current="page" target='blank' href="https://www.moneycontrol.com/india/stockpricequote/finance-nbfc/bajajfinance/BAF"> <strong>SENSEX</strong> </a>
+        </li>
+        <div className="Success">
+        <li className="nav-item">
+          <a className="colo nav-link " href="#">66,589.93</a>
+        </li>
+        </div>
+        <li className="nav-item rolo ">
+          <a className="nav-link" href="#">+529.03(0.80%)</a>
+        </li>
+       
+      </ul>
+
     </div>
+  </div>
+</nav>
+
+
+    </>
   )
 }
 
