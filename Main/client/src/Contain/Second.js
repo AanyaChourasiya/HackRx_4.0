8032080ae2297c820aad4c9291f66cd5f4ef22e4
Ujjawalsahu <ujjawalsahu121@gmail.com>
2023-07-21 17:06:47 +0530

    add third component to client side volume

--- Main/client/src/Contain/Second.js
+++ Main/client/src/Contain/Second.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './first.css'
 function Second(props) {
   return (
     <div>
-      
+        <h2 className='text-center monu my-3 success mx-3' style={{textAlign:"right", fontFamily: "Piazzolla", margin:"none", padding:"none",fontSize:"25px"}} >
+            {props.high}
+        </h2>
+        <h2 className='text-center size-dec success mx-3' style={{textAlign:"right", fontFamily: "Piazzolla", margin:"none", padding:"none",fontSize:"25px"}} >
+            {props.high}
+        </h2>
+
     </div>
   )
 }
